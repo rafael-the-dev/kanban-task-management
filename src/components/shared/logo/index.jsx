262f330a10f5ad7795ea18c7c1c4d14d715e1504
Rafael Tivane <luislangabusiness@gmail.com>
2023-03-24 08:12:21 +0200

    create default logo component

---
+++ src/components/shared/logo/index.jsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+import Hidden from "@mui/material/Hidden";
+import Typography from "@mui/material/Typography";
+
+import classes from "src/components/header/styles.module.css"
+import LogoImage from "public/images/icons/logo-light.svg";
+
+import Link from "../link";
+
+const Logo = (props) => (
+    <Hidden { ...props }>
+        <Link 
+            className={classNames(classes.logoContainer, 'xl:flex items-center')}
+            href="/">
+            <LogoImage />
+            <Typography
+                component="h1"
+                className="font-bold hidden ml-3 text-black text-xl xl:inline-block">
+                Kanban
+            </Typography>
+        </Link>
+    </Hidden>
+);
+
+export default Logo;
